c203bd10b3c9b8c60dc31ddde5f4275362763a57
Nester44 <89927518+Nester44@users.noreply.github.com>
2021-12-02 21:52:51 +0200

    Add files via upload

---
+++ Lab#2.1/titled/cmake-build-debug/CMakeFiles/titled.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/titled.dir/main.c.o"
+  "titled"
+  "titled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/titled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
